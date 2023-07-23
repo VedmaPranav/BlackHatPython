2659b3a4b04552d70654351732c35f856b87f8ff
Pranav <vedmapranav@gmail.com>
2023-07-23 10:14:28 -0400

    bug fixes

--- TCPServer.py
+++ TCPServer.py
@@ -4,10 +4,10 @@ import threading
 bind_ip = "0.0.0.0"
 bind_port = 9999
 
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind((bind_ip,bind_port))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((bind_ip, bind_port))
 server.listen(5)
-print("[*] Listening on %s:%d" % (bind_ip,bind_port))
+print("[*] Listening on %s:%d" % (bind_ip, bind_port))
 
 
 def handle_client(client_socket):
