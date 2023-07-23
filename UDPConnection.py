df65a9eaab72d75c3b7d561fdbe712439c1860f5
pvedma <vedmapranav@gmail.com>
2023-07-23 09:48:40 -0400

    adding TCP Server and modifying clients

--- UDPConnection.py
+++ UDPConnection.py
@@ -1,7 +1,7 @@
 import socket
 
-target_host = "127.0.0.1"
-target_port = 443
+target_host = "0.0.0.0"
+target_port = 9999
 
 client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 client.sendto(b"AAARRRCCC",(target_host,target_port))
